6b749d5cbcd9b2769c72b212d1d750eeb3f0ccd2
d-ivashchuk <dimaiv@me.com>
2021-10-17 14:39:45 +0200

    try fixing pusher on production

--- pusher-config/index.ts
+++ pusher-config/index.ts
@@ -1,7 +1,7 @@
 export const config = {
   appId: "1283284",
   key: "2f3b0570dba46c100272",
-  secret: process.env.PUSHER_SECRET,
+  secret: process.env.NEXT_PUBLIC_PUSHER_SECRET,
   cluster: "eu",
   useTLS: true,
 };
